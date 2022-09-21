bd07b0471649c86dddc7b27fc177bcf2bbfec6b7
Manav Misra <manavm@visionwebsoft.com>
2022-09-21 13:12:54 -0500

    test: :test_tube: pizza toppings should be checked

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("Pizzas", () => {
@@ -29,5 +30,57 @@ describe("Toppings", () => {
 
     // There should be none
     expect(checkedToppings).toHaveLength(0);
+  });
+});
+
+describe("Pizza Toppings", () => {
+  test("Whenever any preset pizza is selected ONLY the associated toppings should be checked", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const pizzaSelect = await screen.findByRole("combobox");
+
+    const cheeseOption = await screen.findByRole("option", { name: /cheese/i });
+    const vegetarianOption = await screen.findByRole("option", {
+      name: /vegetarian/i,
+    });
+    const allInOption = await screen.findByRole("option", { name: /all/i });
+
+    const toppingCheckboxes = await screen.findAllByRole("checkbox");
+
+    user.selectOptions(pizzaSelect, cheeseOption);
+
+    let checked = screen.queryAllByRole("checkbox", { checked: true });
+
+    // Cheese
+    expect(checked).toHaveLength(1);
+    expect(checked[0]).toHaveValue("1");
+
+    user.selectOptions(pizzaSelect, vegetarianOption);
+
+    checked = screen.queryAllByRole("checkbox", { checked: true });
+
+    // Vegetarian - 4 toppings
+    expect(checked).toHaveLength(4);
+    expect(checked[0]).toHaveValue("1");
+    expect(checked[1]).toHaveValue("5");
+    expect(checked[2]).toHaveValue("6");
+    expect(checked[3]).toHaveValue("8");
+
+    user.selectOptions(pizzaSelect, allInOption);
+
+    checked = screen.queryAllByRole("checkbox", { checked: true });
+
+    // All In - 8 toppings
+    expect(checked).toHaveLength(8);
+    expect(checked[0]).toHaveValue("1");
+    expect(checked[1]).toHaveValue("2");
+    expect(checked[2]).toHaveValue("3");
+    expect(checked[3]).toHaveValue("4");
+    expect(checked[4]).toHaveValue("5");
+    expect(checked[5]).toHaveValue("6");
+    expect(checked[6]).toHaveValue("7");
+    expect(checked[7]).toHaveValue("8");
   });
 });
