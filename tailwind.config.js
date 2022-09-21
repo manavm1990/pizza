9ee81dcc059fcae33e3316b6c04ca95383f62915
Manav Misra <manavm@visionwebsoft.com>
2022-09-20 19:15:43 -0500

    chore: :wrench: Tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.jsx"],
-  theme: {
-    extend: {
-      animation: {
-        "App-logo": "spin infinite 20s linear",
-      },
-      colors: {
-        "primary-dark": "#282c34",
-        "primary-link": "#61dafb",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [require("@tailwindcss/forms")],
 };
