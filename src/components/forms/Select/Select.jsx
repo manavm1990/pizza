18424e31889dd5f250b007fe4de3021af9d26f62
Manav Misra <manavm@visionwebsoft.com>
2022-09-20 19:46:17 -0500

    feat: 'Select' renders correctly
    Needs test for selection and value.

---
+++ src/components/forms/Select/Select.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+
+function Select({ choices, label, id, labelOption }) {
+  return (
+    <label htmlFor={id}>
+      {label}
+      <select id={id}>
+        {labelOption && <option value="">{labelOption}</option>}
+        {choices.map((choice) => (
+          <option key={choice.id} value={choice.id}>
+            {choice.name}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+}
+
+Select.propTypes = {
+  choices: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  labelOption: PropTypes.string,
+};
+
+Select.defaultProps = {
+  labelOption: null,
+};
+
+export default Select;
--- src/components/forms/Select/Select.test.jsx
+++ src/components/forms/Select/Select.test.jsx
@@ -3,6 +3,7 @@ import Select from "./Select";
 
 let choices;
 let id;
+let label;
 
 beforeAll(() => {
   choices = [
@@ -12,14 +13,21 @@ beforeAll(() => {
   ];
 
   id = "my-select";
+  label = "My Select";
 });
 
 describe("Select", () => {
   it("renders a select with the correct options (incl. no value option)", () => {
-    const label = "Make a choice";
-    const noValueOption = "--Select a choice--";
+    const labelOption = "--Select a choice--";
 
-    render(<Select choices={choices} label={label} id={id} />);
+    render(
+      <Select
+        choices={choices}
+        label={label}
+        id={id}
+        labelOption={labelOption}
+      />
+    );
 
     const select = screen.getByLabelText(label);
     expect(select).toBeInTheDocument();
@@ -27,7 +35,19 @@ describe("Select", () => {
     const options = screen.getAllByRole("option");
     expect(options).toHaveLength(4);
 
-    expect(options[0]).toHaveTextContent(noValueOption);
-    expect(options[1]).toHaveTextContent("Choice 1");
+    expect(options[0]).toHaveTextContent(labelOption);
+    expect(options[1]).toHaveTextContent(choices[0].name);
+  });
+
+  it("renders a select with the correct options (no no value option)", () => {
+    render(<Select choices={choices} label={label} id={id} />);
+
+    const select = screen.getByLabelText(label);
+    expect(select).toBeInTheDocument();
+
+    const options = screen.getAllByRole("option");
+    expect(options).toHaveLength(3);
+
+    expect(options[0]).toHaveTextContent(choices[0].name);
   });
 });
