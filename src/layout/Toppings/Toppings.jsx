078e0d25128621ff5a448e572297d52eca49bee2
Manav Misra <manavm@visionwebsoft.com>
2022-09-20 22:24:22 -0500

    test: :white_check_mark: 'Toppings'

---
+++ src/layout/Toppings/Toppings.jsx
@@ -0,0 +1,25 @@
+import { Checkbox } from "components/forms";
+import PropTypes from "prop-types";
+
+function Toppings({ toppings }) {
+  return toppings.map((topping) => (
+    <Checkbox
+      key={topping.id}
+      id={topping.id.toString()}
+      label={topping.name}
+      name={topping.id.toString()}
+      handleClick={() => {}}
+    />
+  ));
+}
+
+Toppings.propTypes = {
+  toppings: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Toppings;
