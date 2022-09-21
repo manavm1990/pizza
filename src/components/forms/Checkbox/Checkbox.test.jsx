b771232674894eca6a3410dbf2a38e03c147cd35
Manav Misra <manavm@visionwebsoft.com>
2022-09-20 21:29:32 -0500

    Use variables in checkbox test

--- src/components/forms/Checkbox/Checkbox.test.jsx
+++ src/components/forms/Checkbox/Checkbox.test.jsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Checkbox from "./Checkbox";
 
+const id = "test";
+const label = "Checkbox label";
+const handleClick = () => {};
+
+describe("Checkbox", () => {
 it("renders the proper label", () => {
-  render(<Checkbox label="My Checkbox" />);
+    render(<Checkbox id={id} label={label} handleClick={handleClick} />);
+
+    expect(screen.getByLabelText(label)).toBeInTheDocument();
+  });
+
 
   expect(screen.getByLabelText("My Checkbox")).toBeInTheDocument();
 });
