4e15a5c1a40acdbc6b74bcc00ad54427f27297d7
Manav Misra <manavm@visionwebsoft.com>
2022-09-20 21:19:02 -0500

    Add label and id to Checkbox

--- src/components/forms/Checkbox/Checkbox.jsx
+++ src/components/forms/Checkbox/Checkbox.jsx
@@ -1,16 +1,18 @@
 import PropTypes from "prop-types";
 
-function Checkbox({ label }) {
+function Checkbox({ id, label, handleClick }) {
   return (
-    <label>
-      <input type="checkbox" />
+    <label htmlFor={id}>
+      <input type="checkbox" onClick={handleClick} id={id} />
       {label}
     </label>
   );
 }
 
 Checkbox.propTypes = {
+  id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Checkbox;
