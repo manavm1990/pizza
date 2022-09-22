0454fbc5697d57365ad9e5a58b378f2e19974382
Manav Misra <manavm@visionwebsoft.com>
2022-09-22 07:31:28 -0500

    Add stupid useEffect to track updated prop

--- src/hooks/useChecked.js
+++ src/hooks/useChecked.js
@@ -1,7 +1,11 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function useChecked(isCheckedInitially = false) {
   const [isChecked, setIsChecked] = useState(isCheckedInitially);
+
+  useEffect(() => {
+    setIsChecked(isCheckedInitially);
+  }, [isCheckedInitially]);
 
   const toggle = useCallback(() => setIsChecked((checked) => !checked), []);
   return [isChecked, toggle];
