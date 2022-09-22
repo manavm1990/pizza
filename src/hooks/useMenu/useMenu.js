24e76612acb247de8ede3cf14f3299af03fc9045
Manav Misra <manavm@visionwebsoft.com>
2022-09-22 06:06:13 -0500

    Move useMenu hook to its own directory
    Includes fixing memory leak and other stuff! :(

---
+++ src/hooks/useMenu/useMenu.js
@@ -0,0 +1,51 @@
+import { useEffect, useReducer } from "react";
+import APIService from "services/api";
+
+const api = new APIService();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "initPizzas": {
+      return { ...state, pizzas: action.payload };
+    }
+    case "initToppings": {
+      return { ...state, toppings: action.payload };
+    }
+    case "toggleActiveToppings": {
+      const { pizzaId } = action.payload;
+
+      const selectedPizza = state.pizzas.find(
+        (pizza) => pizza.id === Number(pizzaId)
+      );
+
+      const updatedToppings = state.toppings.map((topping) => {
+        const isActive = selectedPizza.toppingIds.includes(topping.id);
+
+        return { ...topping, isActive };
+      });
+
+      return { ...state, toppings: updatedToppings };
+    }
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+};
+
+function useMenu() {
+  const [state, dispatch] = useReducer(reducer, {});
+  console.log(state, "menu");
+
+  useEffect(() => {
+    api.index("pizzas").then((data) => {
+      dispatch({ type: "initPizzas", payload: data });
+    });
+
+    api.index("toppings").then((data) => {
+      dispatch({ type: "initToppings", payload: data });
+    });
+  }, []);
+
+  return [state, dispatch];
+}
+
+export default useMenu;
